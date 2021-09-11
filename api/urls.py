8ec14c30d75a5e056d77b5c0b1c5512990e25edb
ali <gediali019@gmail.com>
2021-09-11 16:48:04 +0300

    added cmponenet to redirect user to room

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.RoomView.as_view()),
     path('create-room', views.CreateRoomView.as_view()),
     path('get-room', views.GetRoom.as_view()),
-    path('join-room', views.JoinRoom.as_view())
+    path('join-room', views.JoinRoom.as_view()),
+    path('user-in-room', views.UserInRoom.as_view()),
 ]
