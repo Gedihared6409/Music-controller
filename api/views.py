12524a5f677f21dac9106a635a070a92bdf2bc34
ali <gediali019@gmail.com>
2021-09-12 20:57:40 +0300

    added function for living room

--- api/views.py
+++ api/views.py
@@ -88,6 +88,18 @@ class UserInRoom(APIView):
         }
         print(data)
         return JsonResponse(data, status=status.HTTP_200_OK)
+class LeaveRoom(APIView):
+    def post(self, request, format=None):
+        if 'room_code' in self.request.session:
+            self.request.session.pop('room_code')
+            host_id = self.request.session.session_key
+            room_results = Room.objects.filter(host=host_id)
+            if len(room_results) > 0:
+                room = room_results[0]
+                room.delete()
+
+        return Response({'Message': 'Success'}, status=status.HTTP_200_OK)
+
 
 # from operator import mul
 # from functools import reduce
